3245f93cc575308f9b415deff46fd7ac08f26f2b
KWOK William <kwokw@co.multnomah.or.us>
2017-02-19 17:21:06 -0800

    Fixed test error

--- src/Tests/IdentityRoleTests.cs
+++ src/Tests/IdentityRoleTests.cs
@@ -35,8 +35,7 @@
 
 			var role = new IdentityRole(name);
 
-			//Expect(role.Name, Is.EqualTo(name));
-            Expect(role.Name, Is.EqualTo("notadmin"));
+			Expect(role.Name, Is.EqualTo(name));
 		}
 
 		[Test]
